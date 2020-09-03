4f1eb10116c83ced0dd61bb70fd9b283dcb85204
Tri Tran <tritranit7121@gmail.com>
2020-09-03 19:58:10 +0700

    Add configuration

--- src/components/CategoryGrid.js
+++ src/components/CategoryGrid.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 import {FlatGrid} from 'react-native-super-grid';
-import CategoryItem from './CategoryItem';
-import {STRINGS} from '../assets/localize/String';
 import {ICONS} from '../assets/icons';
+import {STRINGS} from '../assets/localize/String';
 import {Colors, Helpers} from '../theme';
+import CategoryItem from './CategoryItem';
 
 const CATEGORIES = [
   STRINGS.common.business,
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+import {API_KEY, BASE_URL} from 'react-native-config';
+
+export default {
+  API_KEY,
+  BASE_URL,
+};
